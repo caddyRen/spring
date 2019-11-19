4877d544eb6e2826c20a75d562a936cf566bc24b
renqiankun <18819284248@163.com>
2019-11-19 09:06:58 +0800

    react study

---
+++ demo/src/main/resources/static/js/hi.js
@@ -0,0 +1,12 @@
+const e=React.createElement;
+const str='李亚云 Is a Fucking Fog'
+const vDom=e('h1',{id:'id'},str.toUpperCase());
+const vDom1=e('h2',{id:'id'},str.toLowerCase());
+const vDom2=e('h3',{id:'id'},str);
+const vDom3=document.querySelector('#test2');
+const vDom4=e('button', {onClick: () => this.setState({liked:true})},'like');
+
+// ReactDOM.render(e(LikeButton),vDom2);
+ReactDOM.render(vDom,document.getElementById('test'));
+ReactDOM.render(vDom1,document.getElementById('test2'));
+ReactDOM.render(vDom2,document.getElementById('test3'));
