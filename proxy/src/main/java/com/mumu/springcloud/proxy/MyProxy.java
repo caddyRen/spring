ddac22fd26ef49f788f86012100bafe721350468
renqiankun <18819284248@163.com>
2019-11-19 09:37:35 +0800

    add Proxy module

---
+++ proxy/src/main/java/com/mumu/springcloud/proxy/MyProxy.java
@@ -0,0 +1,16 @@
+package com.mumu.springcloud.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author renqiankun
+ */
+@SpringBootApplication
+@ServletComponentScan
+public class MyProxy {
+    public static void main(String[] args) {
+        SpringApplication.run(MyProxy.class, args);
+    }
+}
---
+++ proxy/src/main/java/com/mumu/springcloud/proxy/config/SolrProxyServletConfiguration.java
@@ -0,0 +1,50 @@
+package com.mumu.springcloud.proxy.config;
+
+import com.mumu.springcloud.proxy.filter.ApiRequestLoggingFilter;
+import com.mumu.springcloud.proxy.utils.ProxyServlet;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * @author renqiankun
+ */
+@Configuration
+public class SolrProxyServletConfiguration implements EnvironmentAware {
+
+    @Bean
+    @Order(0)
+    public ServletRegistrationBean servletRegistrationBean(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(), propertyResolver.getProperty("servlet_url"));
+        servletRegistrationBean.addInitParameter("targetUri", propertyResolver.getProperty("target_url"));
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, propertyResolver.getProperty("logging_enabled", "true"));
+        return servletRegistrationBean;
+    }
+    private RelaxedPropertyResolver propertyResolver;
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.propertyResolver = new RelaxedPropertyResolver(environment, "proxy.solr.");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "requestLoggingFilter")
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new ApiRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setMaxPayloadLength(1024 * 1024 * 2);
+        loggingFilter.setBeforeMessagePrefix("接收请求 [");
+        loggingFilter.setBeforeMessageSuffix("]");
+        loggingFilter.setAfterMessagePrefix("结束请求 [");
+        loggingFilter.setAfterMessageSuffix("]");
+        return loggingFilter;
+    }
+}
