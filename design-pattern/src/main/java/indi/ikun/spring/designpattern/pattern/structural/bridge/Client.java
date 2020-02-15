12d59d0a78d8887b1f6da6cf43f9dab391051415
caddyRen <18819284248@163.com>
2020-02-16 00:08:28 +0800

    设计模式=======>桥接模式

---
+++ design-pattern/src/main/java/indi/ikun/spring/designpattern/pattern/structural/bridge/Client.java
@@ -0,0 +1,34 @@
+package indi.ikun.spring.designpattern.pattern.structural.bridge;
+
+/**
+ * @Description: TODO
+ * @Author caddy
+ * @date 2020-02-12 14:13:50
+ * @version 1.0
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Phone p=new UpRightPhone(new Xiaomi());
+        p.open();
+        p.call();
+        p.close();
+        System.err.println("----");
+        p=new UpRightPhone(new Iphone());
+        p.open();
+        p.call();
+        p.close();
+        System.err.println("----");
+        p=new FoldedPhone(new Iphone());
+        p.open();
+        p.call();
+        p.close();
+        System.err.println("----");
+        p=new FoldedPhone(new Xiaomi());
+        p.open();
+        p.call();
+        p.close();
+
+    }
+
+}
