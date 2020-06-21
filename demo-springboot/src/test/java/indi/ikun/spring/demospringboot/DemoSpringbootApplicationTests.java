16988619129b2d18ebd07fc07ad2d240286f7ab5
renqiankun <18819284248@163.com>
2020-06-22 01:18:35 +0800

    优化gradle配置

--- demo-springboot/src/test/java/indi/ikun/spring/demospringboot/DemoSpringbootApplicationTests.java
+++ demo-springboot/src/test/java/indi/ikun/spring/demospringboot/DemoSpringbootApplicationTests.java
@@ -1,5 +1,7 @@
 package indi.ikun.spring.demospringboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,5 +13,11 @@ import org.springframework.test.context.junit4.SpringRunner;
   **/
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class DemoSpringbootApplicationTests {
+
+    @Test
+    public void test(){
+        log.info("111");
+    }
 }
