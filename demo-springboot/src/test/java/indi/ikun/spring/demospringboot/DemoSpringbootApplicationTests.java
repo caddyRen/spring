232190905bc5a274978e62f665cadb52031e528c
renqiankun <18819284248@163.com>
2020-06-22 11:23:40 +0800

    CRLF to LF

--- demo-springboot/src/test/java/indi/ikun/spring/demospringboot/DemoSpringbootApplicationTests.java
+++ demo-springboot/src/test/java/indi/ikun/spring/demospringboot/DemoSpringbootApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
-  *@TODO //启动类
   *@Author:renqiankun
   *@Date:2020-01-08 10:55
   **/
