c7971f837d8085f986e50292ef4e9a9ba5452553
renqiankun <18819284248@163.com>
2020-07-10 17:36:25 +0800

    eureka 容器化 config-server 容器化

---
+++ config-server/bashfiles/define.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service_name="config-server"
+version="1.0.0-SNAPSHOT"
+port="8761"
---
+++ eureka-server/bashfiles/define.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service_name="eureka-server"
+version="1.0.0-SNAPSHOT"
+port="8761"
