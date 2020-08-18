4e955c7333278808410302a50d9013434ea26712
renqiankun <18819284248@163.com>
2020-08-18 22:34:19 +0800

    java编码的151个优秀建议-151

---
+++ base_java/src/main/java/indi/ikun/spring/basejava/codequality/chapter03/package-info.java
@@ -0,0 +1,9 @@
+package indi.ikun.spring.basejava.codequality.chapter03;
+class PkgConst{
+    static final String PACKAGE_CONST="ABC";
+}
+class PkgClass{
+    public void test(){
+        System.err.println(12);
+    }
+}
---
+++ base_java/src/main/java/indi/ikun/spring/basejava/codequality/chapter12/Fo.java
@@ -0,0 +1,13 @@
+package indi.ikun.spring.basejava.codequality.chapter12;
+
+/**
+ * 144.提倡良好的代码风格
+ * 1）整洁
+ * 2）统一
+ * 3）流行
+ * 4）便捷
+ */
+public class Fo {
+
+}
+
