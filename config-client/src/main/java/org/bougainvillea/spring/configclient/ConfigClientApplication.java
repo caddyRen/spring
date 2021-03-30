1d51546034b871ef6a90e5fa6c4dbd5cdecbb10f
caddy <18819284248@163.com>
2021-03-30 20:39:49 +0800

    feat: config client demo

---
+++ config-client/src/main/java/org/bougainvillea/spring/configclient/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package org.bougainvillea.spring.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@RefreshScope
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
